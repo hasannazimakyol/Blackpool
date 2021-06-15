168586e58eb156bb7f1019d7cbe47dcd78a775b2
hasannazimakyol <hnakyol@eskisehir.edu.tr>
2021-06-15 14:39:50 +0300

    Third commit

--- webapp/controller/PlayersList.controller.js
+++ webapp/controller/PlayersList.controller.js
@@ -148,6 +148,7 @@ sap.ui.define([
 			var newPosition = this.getView().byId("myComboBox").getSelectedItem() ?
 				this.getView().byId("myComboBox").getSelectedItem().getText() : "Unspecified";
 
+			//this.newRecord = {};
 			this.newRecord = {};
 
 			this.newRecord = {
